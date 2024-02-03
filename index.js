8b944c874319acba4cb258f41bba10d036e76f93
Adarsh Thakur <58141006+ADARSH770@users.noreply.github.com>
2024-02-03 21:52:13 +0530

    Update index.js by adding condition for empty input box

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+    
 function res(){
     let con2 = document.getElementById('images')
     while(con2.firstChild){
@@ -6,6 +6,7 @@ function res(){
     }
     
     let val = document.getElementById('input').value
+    let val2 = document.getElementById('input')
     let api =`http://api.qrserver.com/v1/create-qr-code/?data=${val}&size=100x100`;
     let ele = document.createElement("img")
     ele.style.marginLeft="110px"
@@ -16,6 +17,16 @@ function res(){
     ele.style.borderRadius="3px"
     ele.setAttribute("src",api)
     let con = document.getElementById('images')
+    
+    if(val.length>0){
     con.appendChild(ele)
+    }else{
+        console.log("shake");
+      
+        setTimeout(()=>{
+            val2.classList.toggle("error")
+        },200)
+    }
+
 }
 
